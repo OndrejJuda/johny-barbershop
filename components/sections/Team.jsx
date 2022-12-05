8bbf741f8ac9c5673ac264783aac7d2db73c7a57
Ondrej Juda <ondrej.juda@networg.com>
2022-12-05 21:17:58 +0100

    team section

--- components/sections/Team.jsx
+++ components/sections/Team.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import { SectionWrapper } from '../';
 
+const TEAM = [
+  { name: 'Carlos Santiago', photoUrl: '/barber1.jpg', instagramUrl: '' },
+  { name: 'Peter Green', photoUrl: '/barber2.jpg', instagramUrl: '' },
+  { name: 'Jan Maličký', photoUrl: '/barber3.jpg', instagramUrl: '' },
+];
+
 const Team = () => {
   return (
     <SectionWrapper id='team' title='Tým' description='Seznam se s našimi holiči'>
-      Team
+      <div className='flex gap-24 flex-wrap'>
+        {
+          TEAM.map(({ photoUrl, name, instagramUrl }, index) => (
+            <article
+              key={index}
+              className='relative group border border-primary-500 p-2 '
+            >
+              <img src={photoUrl} alt={name} className='w-full h-full object-cover w-96 h-[500px]' />
+              <div
+                className='w-96 h-[80%] absolute bottom-0
+                transition opacity-0 group-hover:opacity-100  
+                bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0)]'
+              />
+              <p
+                className='absolute bottom-[20%] left-[50%] -translate-x-[50%] 
+              text-2xl text-primary-100
+              transition opacity-0 group-hover:opacity-100'
+              >{name}</p>
+            </article>
+          ))
+        }
+      </div>
     </SectionWrapper>
   );
 }
