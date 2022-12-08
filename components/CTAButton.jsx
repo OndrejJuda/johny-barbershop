bfc86f838be285a2a8ce3f18dab96cb577aa65aa
Ondrej Juda <ondrej.juda@networg.com>
2022-12-08 07:19:30 +0100

    services

--- components/CTAButton.jsx
+++ components/CTAButton.jsx
@@ -6,8 +6,8 @@ const CTAButton = () => {
   return (
     <Link href='#contact'>
       <div className='group'>
-        <Frame style='transition group-hover:p-[4px] group-hover:my-[4px]
-        group-active:p-[6px] group-active:my-[2px]'>
+        <Frame style='transition group-hover:p-[4px] group-hover:m-[4px]
+        group-active:p-[6px] group-active:m-[2px]'>
           <button
             type='button'
             className='text-primary-300 text-2xl font-light bg-primary-50 bg-opacity-20 p-4
--- components/sections/Services.jsx
+++ components/sections/Services.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
-import { SectionWrapper } from '../wrappers';
+import { SectionWrapper, Frame } from '../wrappers';
+import { CTAButton } from '../';
+
+const SERVICES = [
+  { name: 'Střih', price: 450 },
+  { name: 'Střih a mytí', price: 540 },
+  { name: 'Péče o dlouhé vlasy', price: 680 },
+  { name: 'Úprava boků strojkem', price: 400 },
+  { name: 'Zastřížení', price: 380 },
+  { name: 'Holení', price: 580 },
+  { name: 'Holení celé hlavy', price: 430 },
+  { name: 'Péče o pleť', price: 500 },
+  { name: 'Styling', price: 500 },
+];
+
+
+
+const CZK_FORMAT = new Intl.NumberFormat(
+  'cs-CZ',
+  { style: 'currency', currency: 'CZK' }
+);
 
 const Services = () => {
   return (
     <SectionWrapper id='services' title='Služby' description='Kvalitní péče o vlasy a vousy'>
-      Services
+      <Frame hasDecoration>
+        <div className='p-16 flex flex-col items-center'>
+          <ul className='mb-8'>
+            {
+              SERVICES.map(({ name, price }, index) => (
+                <li
+                  key={index}
+                  className='text-primary-500 text-xl font-light flex gap-1'
+                >
+                  <p>{name}</p>
+                  <span className='flex-1 min-w-[150px] border-b border-dotted mb-[6px]' />
+                  <p>{CZK_FORMAT.format(price)}</p>
+                </li>
+              ))
+            }
+          </ul>
+          <CTAButton />
+        </div>
+      </Frame>
     </SectionWrapper>
   );
 }
