59cbf0192286693f64bbf71a9d99181fe5f241a7
Ondrej Juda <ondrej.juda@networg.com>
2022-12-15 17:14:31 +0100

    mobile header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { RiScissors2Line } from 'react-icons/ri';
 
-const NavLink = ({ href, title }) => (
-  <Link href={href}>
+const NavLink = ({ href, title, setIsOpened }) => (
+  <Link
+    href={href}
+    onClick={() => {
+      setIsOpened && setIsOpened(false)
+    }}>
     <div className='group flex justify-center flex-col'>
       <p className='text-2xl transition group-hover:text-primary-500 mb-1 text-center'>{title}</p>
       <img
@@ -14,10 +19,12 @@ const NavLink = ({ href, title }) => (
   </Link>
 );
 
-const Header = ({bgColor}) => {
+const Header = ({ bgColor }) => {
+  const [isOpened, setIsOpened] = useState(false);
+
   return (
-    <header className={`${bgColor} text-primary-300 font-primary font-light hidden xl:block`}>
-      <nav className='flex justify-center items-center gap-44 h-40'>
+    <header className={`${bgColor} text-primary-300 font-primary font-light`}>
+      <nav className='justify-center items-center gap-44 h-40 hidden xl:flex'>
         <NavLink href='#team' title='Tým' />
         <NavLink href='#services' title='Služby' />
         <Link href='/'>
@@ -30,6 +37,31 @@ const Header = ({bgColor}) => {
         <NavLink href='#news' title='Novinky' />
         <NavLink href='#contact' title='Kontakt' />
       </nav>
+      <>
+        <button
+          type='button'
+          className={`flex xl:hidden fixed z-30 top-5 left-5 
+          bg-primary-50 text-primary-700 rounded-full p-4 
+          transition ${isOpened ? 'rotate-180' : ''}`}
+          onClick={() => setIsOpened((prevValue) => !prevValue)}
+        >
+          <RiScissors2Line size={48} />
+        </button>
+        <div className={`bg-primary-900 opacity-100 fixed z-20 transition ${isOpened ? 'w-screen h-screen block' : 'hidden'}`}> </div>
+        <nav className={`${isOpened ? 'flex' : 'hidden'} xl:hidden w-screen h-screen z-20 fixed justify-center items-center flex-col gap-24`}>
+          <Link href='/' onClick={() => setIsOpened(false)}>
+            <img
+              alt='logo'
+              src='/logo.png'
+              className='w-48'
+            />
+          </Link>
+          <NavLink href='#team' title='Tým' setIsOpened={setIsOpened} />
+          <NavLink href='#services' title='Služby' setIsOpened={setIsOpened} />
+          <NavLink href='#news' title='Novinky' setIsOpened={setIsOpened} />
+          <NavLink href='#contact' title='Kontakt' setIsOpened={setIsOpened} />
+        </nav>
+      </>
     </header>
   );
 }
